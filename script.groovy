96e78256f49e919d2e281d58bf0a0cc8210baa01
djangoreactdev <djangoreactdev@gmail.com>
2023-06-07 13:57:27 +0000

    add tag

--- script.groovy
+++ script.groovy
@@ -19,8 +19,11 @@ def buildImage() {
     withCredentials([usernamePassword(credentialsId: 'DockerHub', passwordVariable: 'PASSWORD', usernameVariable: 'USERNAME')]) {
 
         sh 'echo $PASSWORD | docker login -u $USERNAME --password-stdin'
+        sh 'docker tag cosmetic-front:1.0 djangoreactdev/cosmetic-front:1.0'
         sh 'docker push djangoreactdev/cosmetic-front:1.0'
+        sh 'docker tag cosmetic-api:1.0 djangoreactdev/cosmetic-api:1.0'
         sh 'docker push djangoreactdev/cosmetic-api:1.0'
+        sh 'docker tag cosmetic-dashboard:1.0 djangoreactdev/cosmetic-dashboard:1.0'
         sh 'docker push djangoreactdev/cosmetic-dashboard:1.0'
     }
 } 
