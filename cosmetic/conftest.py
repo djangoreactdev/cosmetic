0be1bb66a973db28ae91182510bd07ec7e4aeb02
djangoreactdev <djangoreactdev@gmail.com>
2023-06-07 12:00:12 +0000

    add dashboard api and front

---
+++ cosmetic/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from cosmetic.users.models import User
+from cosmetic.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user(db) -> User:
+    return UserFactory()
--- script.groovy
+++ script.groovy
@@ -5,21 +5,26 @@ def buildJar() {
 
 def buildImage() {
     echo "building the docker image..."
+    // sh 'docker build -t djangoreactdev/cosmetic:1.0 ./front-next'
+    // sh 'docker build -t djangoreactdev/cosmetic-api:1.0 ./compose/local/django'
+    sh 'docker compose -f production.yml build django front dashboard'
     withCredentials([usernamePassword(credentialsId: 'DockerHub', passwordVariable: 'PASSWORD', usernameVariable: 'USERNAME')]) {
-        sh 'docker build -t djangoreactdev/cosmetic:1.0 ./front-next'
+
         sh 'echo $PASSWORD | docker login -u $USERNAME --password-stdin'
-        sh 'docker push djangoreactdev/cosmetic:1.0'
+        sh 'docker push djangoreactdev/cosmetic-front:1.0'
+        sh 'docker push djangoreactdev/cosmetic-api:1.0'
+        sh 'docker push djangoreactdev/cosmetic-dashboard:1.0'
     }
 } 
 
-def deployApp() {
-    echo 'deploying the application...'
-    sh 'docker pull djangoreactdev/cosmetic:1.0'
-    sh 'docker stop cosmetic || true'
-    sh 'docker rm cosmetic || true'
-    sh 'docker rmi cosmetic || true'
-    sh 'docker pull djangoreactdev/cosmetic:1.0'
-    sh 'docker run -d --name cosmetic -p 82:3000 djangoreactdev/cosmetic:1.0'
-} 
+// def deployApp() {
+//     echo 'deploying the application...'
+//     sh 'docker pull djangoreactdev/cosmetic:1.0'
+//     sh 'docker stop cosmetic || true'
+//     sh 'docker rm cosmetic || true'
+//     sh 'docker rmi cosmetic || true'
+//     sh 'docker pull djangoreactdev/cosmetic:1.0'
+//     sh 'docker run -d --name cosmetic -p 82:3000 djangoreactdev/cosmetic:1.0'
+// } 
 
 return this
