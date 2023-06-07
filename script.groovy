dc1bb6b52f690821098c21f45189495fd1a685a4
djangoreactdev <djangoreactdev@gmail.com>
2023-06-07 12:40:53 +0000

    env file configyration

--- script.groovy
+++ script.groovy
@@ -7,6 +7,25 @@ def buildImage() {
     echo "building the docker image..."
     // sh 'docker build -t djangoreactdev/cosmetic:1.0 ./front-next'
     // sh 'docker build -t djangoreactdev/cosmetic-api:1.0 ./compose/local/django'
+
+    withCredentials([
+                    file(credentialsId: 'env_file_django', variable: 'ENV_django')
+                    file(credentialsId: 'env_file_postgres', variable: 'ENV_postgres')
+                ]) {
+                    sh '
+                        # django env
+                        # Extract the contents of the credentials file to a temporary directory
+                        tmp_dir=$(mktemp -d)
+                        tar -xf ${ENV_django} -C ${tmp_dir}
+
+                        # Copy the files from the temporary directory to your desired location
+                        cp -R ${tmp_dir}/folder_with_files /.envs/.production/.django
+
+                        # postgres env
+                        tar -xf ${ENV_postgres} -C ${tmp_dir}
+                        cp -R ${tmp_dir}/folder_with_files /.envs/.production/.postgres
+                  '
+                }
     sh 'docker compose -f production.yml build django front dashboard'
     withCredentials([usernamePassword(credentialsId: 'DockerHub', passwordVariable: 'PASSWORD', usernameVariable: 'USERNAME')]) {
 
