f019c545df4855074c26a753c292336516f3e1f7
djangoreactdev <djangoreactdev@gmail.com>
2023-06-16 12:30:07 +0000

    add nginx to production

--- config/urls.py
+++ config/urls.py
@@ -23,7 +23,7 @@ urlpatterns = [
 
 urlpatterns += [
     # path("graphql/", csrf_exempt(GraphQLView.as_view(graphiql=True))),
-    path("graphql/", csrf_exempt(FileUploadGraphQLView.as_view(graphiql=True))),
+    path("api/graphql", csrf_exempt(FileUploadGraphQLView.as_view(graphiql=True))),
     path("charts/", include("cosmeticpro.ecommerce.charts.urls")),
 ]
 
