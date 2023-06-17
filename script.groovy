7e3433117deb416492dc915677451b5382b54274
djangoreactdev <djangoreactdev@gmail.com>
2023-06-17 10:15:31 +0000

    add deploying to production not end code

--- script.groovy
+++ script.groovy
@@ -5,8 +5,6 @@ def buildJar() {
 
 def buildImage() {
     echo "building the docker image..."
-    // sh 'docker build -t djangoreactdev/cosmetic:1.0 ./front-next'
-    // sh 'docker build -t djangoreactdev/cosmetic-api:1.0.2 ./compose/local/django'
 
     withCredentials([
             file(credentialsId: 'env_file_cosmetic_django', variable: 'ENV_cosmetic_django'),
@@ -28,14 +26,43 @@ def buildImage() {
     }
 } 
 
-// def deployApp() {
-//     echo 'deploying the application...'
-//     sh 'docker pull djangoreactdev/cosmetic:1.0'
-//     sh 'docker stop cosmetic || true'
-//     sh 'docker rm cosmetic || true'
-//     sh 'docker rmi cosmetic || true'
-//     sh 'docker pull djangoreactdev/cosmetic:1.0'
-//     sh 'docker run -d --name cosmetic -p 82:3000 djangoreactdev/cosmetic:1.0'
-// } 
+def deployApp() {
+    echo 'deploying the application...'
+    sh 'docker stack rm cosmetic || true'
+    sh 'docker system prune -f'
+
+
+    // cosmetic
+    withCredentials([
+            file(credentialsId: 'env_file_cosmetic_django', variable: 'ENV_cosmetic_django'),
+            file(credentialsId: 'env_file_cosmetic_postgres', variable: 'ENV_cosmetic_postgres')
+        ]) {
+            writeFile file: 'cosmetic/.envs/.production/.django', text: readFile(ENV_cosmetic_django)
+            writeFile file: 'cosmetic/.envs/.production/.postgres', text: readFile(ENV_cosmetic_postgres)
+        }
+
+
+    withCredentials([usernamePassword(credentialsId: 'DockerHub', passwordVariable: 'PASSWORD', usernameVariable: 'USERNAME')]) {
+
+        sh 'echo $PASSWORD | docker login -u $USERNAME --password-stdin'
+        sh 'docker compose -f production.yml build'
+        sh 'docker compose -f production.yml pull --ignore-pull-failures'
+    
+    }
+
+    sh '''
+        #!/bin/bash
+
+        NETWORK_NAME="cosmetic_default"
+        if ! docker network ls | grep -q "$NETWORK_NAME"; then
+            # If the network doesn't exist, create it
+            docker network create --driver overlay --scope swarm cosmetic_default
+        fi
+            echo "Network $NETWORK_NAME exists"
+        '''
+
+    sh 'docker stack deploy -c production.yml cosmetic'
+
+} 
 
 return this
